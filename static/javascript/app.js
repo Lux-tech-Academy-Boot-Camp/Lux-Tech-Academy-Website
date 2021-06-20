79e8e13283acb36d0a24a953db80ed86f7402c83
MaryMaina98 <marieshix98@gmail.com>
2021-06-20 22:43:32 +0300

    -Ensures the web page is responsive on smaller screens

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,12 +1,30 @@
-let showmenu  = document.querySelector(".menu");
+// let showmenu  = document.querySelector(".menu");
 
-function show() {
-	showmenu.style.display = "block";
+// function show() {
+// 	showmenu.style.display = "block";
 
-} 
+// } 
 
-window.onclick = function(event) {
-  if (event.target == showmenu ) {
-    showmenu.style.display = "none";
-  }
+// window.onclick = function(event) {
+//   if (event.target == showmenu ) {
+//     showmenu.style.display = "none";
+//   }
+// }
+
+const showMenu = ()=>{
+const menu= document.getElementById('icon');
+const navLinks = document.getElementById('menu')
+menu.addEventListener('click', ()=>{
+  navLinks.classList.toggle('open-menu')
+})
 }
+showMenu()
+//sticky navbar
+
+window.addEventListener('scroll', ()=>{
+const navBar= document.querySelector('.main-nav');
+const content = document.querySelector('.md-content')
+navBar.classList.toggle('sticky',window.scrollY > 0)
+content.style.position ='fixed'
+console.log('Sticky')
+})
